fe65ae027cba3b7009840104bd54df64152ee0d5
Aashish Bista <aashishbista@Aashishs-MacBook-Pro.local>
2026-01-21 18:36:52 -0600

    Hotfix f3() method in all classes (Version 3.1)

--- Class2.java
+++ Class2.java
@@ -13,7 +13,7 @@ public class Class2 {
     // v2.0
     }
     private void f3() {
-    // v3.0
+    // v3.0 - hotfix applied
     }
     private void f4() {
         // v4.0
--- HelloWorld.java
+++ HelloWorld.java
@@ -19,8 +19,8 @@ public class HelloWorld {
     // v2.0
     }
     private void f3() {
-    // v3.0
-   }
+    // v3.0 - hotfix applied
+    }
     private void f4() {
         // v4.0
 }
